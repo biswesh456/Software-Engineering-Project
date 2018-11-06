7805248d4b3b9d25d65e1f10486bd464e79d4d75
biswesh456 <biswesh.mohapatra@iiitb.org>
2018-11-06 23:56:15 +0530

    Add test model

---
+++ Pathago/app/models/test.rb
@@ -0,0 +1,3 @@
+class Test < ApplicationRecord
+  has_many :pathogy_lab_tests
+end
--- Pathago/db/schema.rb
+++ Pathago/db/schema.rb
@@ -10,7 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181104120613) do
+ActiveRecord::Schema.define(version: 20181106181514) do
+
+  create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "pathology_lab_tests", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "pathologylab_id"
+    t.bigint "test_id"
+    t.index ["pathologylab_id"], name: "index_pathology_lab_tests_on_pathologylab_id"
+    t.index ["test_id"], name: "index_pathology_lab_tests_on_test_id"
+  end
 
   create_table "pathologylabs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
@@ -50,4 +77,12 @@ ActiveRecord::Schema.define(version: 20181104120613) do
     t.index ["reset_password_token"], name: "index_patients_on_reset_password_token", unique: true
   end
 
+  create_table "tests", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "pathology_lab_tests", "pathologylabs"
+  add_foreign_key "pathology_lab_tests", "tests"
 end
