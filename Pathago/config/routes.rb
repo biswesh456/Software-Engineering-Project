f2c9285f5785cec6df5bd63bdfa9be92c0477949
biswesh456 <biswesh.mohapatra@iiitb.org>
2018-11-11 16:05:19 +0530

    Add bootstrap to devise

--- Pathago/config/routes.rb
+++ Pathago/config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   get "/", to: "index#index"
-  devise_for :admins, path: 'admin', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }, controllers: {registrations: 'admins/registrations'}
+  devise_for :admins, path: 'admin', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }, controllers: {registrations: 'admins/registrations', sessions: 'admins/sessions'}
   devise_scope :admin do
    get "signup", to: "admins/registrations#new"
    get "login", to: "admins/sessions#new"
    get "logout", to: "admins/sessions#destroy"
   end
-  devise_for :patients, path: 'patient', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }, controllers: {registrations: 'patients/registrations'}
+  devise_for :patients, path: 'patient', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }, controllers: {registrations: 'patients/registrations', sessions: 'patients/sessions', passwords: 'patients/passwords'}
   devise_scope :patient do
    get "signup", to: "patients/registrations#new"
    get "login", to: "patients/sessions#new"
    get "logout", to: "patients/sessions#destroy"
   end
-  devise_for :pathologylabs, path: 'pathologylab', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }, controllers: {registrations: 'pathologylabs/registrations'}
+  devise_for :pathologylabs, path: 'pathologylab', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }, controllers: {registrations: 'pathologylabs/registrations', sessions: 'pathologylabs/sessions', passwords: 'pathologylabs/passwords'}
   devise_scope :pathologylab do
    get "signup", to: "pathologylabs/registrations#new"
    get "login", to: "pathologylabs/sessions#new"
